a829123c483bfcef33633528a4a1e7a433ceef80
John Sarracino <jsarraci@cs.ucsd.edu>
2017-03-16 17:45:50 -0700

    added readme for scripts

--- fetchbot/scripts/output_tracking.sh
+++ fetchbot/scripts/output_tracking.sh
@@ -3,11 +3,11 @@
 # ././devel/.private/mars_ros/lib/mars_ros/micarray_recorder ls -1* raw
 TRACK_DIR=/home/pi/workspaces/mars_ros/build/mars/devel/lib/mars/demo
 CONFIG=/home/pi/workspaces/mars_ros/src/mars/config/matrixcreator.cfg
-# PRE_DIR=/tmp/fetchbot/
-PRE_DIR=/home/pi/workspaces/mars_ros/src/
+PRE_DIR=/tmp/fetchbot/
+#PRE_DIR=/home/pi/workspaces/mars_ros/src/
 
 # ./build/mars/devel/lib/mars/demo -i src/mars_ros/mars_ros/src/channel_all.raw -c src/mars/config/matrixcreator.cfg -v
 # 2.7s
 # echo $
-trackarg=$TRACK_DIR" -i "$PRE_DIR"channel_all.raw -c "$CONFIG" -v > "$PRE_DIR"tracking.out" 
+trackarg=$TRACK_DIR" -i "$PRE_DIR"channel_all.raw -c "$CONFIG" -v > "$PRE_DIR"tracking.out"
 eval $trackarg
